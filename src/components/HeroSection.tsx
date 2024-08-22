f7389ad0a52dec5a883a2a6bb0f54fe2313883f6
ARTI <128461357+manitra-rasolofoarimanga@users.noreply.github.com>
2024-08-22 11:01:04 +0300

    Fix CV URL

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -32,7 +32,7 @@ const HeroSection = () => {
                             from Madagascar. I'll start my final year of Master's studies soon and am deeply passionate about programming.<br /> Find out more about me by clicking on the button below.
                         </Typography>
                         <div className="flex lg:inline-block justify-center lg:justify-normal w-full lg:w-auto">
-                            <a href="/cv-manitra_rasolofoarimanga.pdf" target="_blank" rel="noopener noreferrer">
+                            <a href="/portfolio/cv-manitra_rasolofoarimanga.pdf" target="_blank" rel="noopener noreferrer">
                                 <Button variant="text" className="flex items-center bg-gray-900/10 hover:bg-gray-900/20 gap-2 text-center lg:text-left">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
--- src/pages/MyStackPage.tsx
+++ src/pages/MyStackPage.tsx
@@ -41,7 +41,7 @@ const MyStackPage = () => {
                     I'm mainly proficient in Java, JavaScript, Python and PHP, as well as the frameworks associated with each of these languages. I also have solid expertise in databases, particularly SQL. For more details on my skills and experience, please see my CV.
                 </Typography>
                 <div data-aos="zoom-in" className="flex justify-center w-full lg:w-auto mt-4">
-                    <a href="/cv-manitra_rasolofoarimanga.pdf" target="_blank" rel="noopener noreferrer">
+                    <a href="/portfolio/cv-manitra_rasolofoarimanga.pdf" target="_blank" rel="noopener noreferrer">
                         <Button variant="text" className="flex items-center bg-gray-900/10 hover:bg-gray-900/20 gap-2 text-center lg:text-left">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
